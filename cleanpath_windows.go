f0c62386d83635b4deb502f13f72cbd3092d8a5d
Xabier Eizmendi <xeizmendi@gmail.com>
2018-09-03 17:02:40 +0200

    Include sftp client hack

--- cleanpath_windows.go
+++ cleanpath_windows.go
@@ -1,10 +1,18 @@
 package sftp
 
-import "path/filepath"
+import (
+	"path/filepath"
+	"strings"
+)
 
 // Makes sure we have a clean Windows absolute path to work with
 // If the path is not absolute we assume it's on C:\
 func cleanPath(p string) string {
+	// Hack to make path sent by sftp client work in Windows
+	if absPath, err := filepath.Abs("."); err == nil {
+		p = strings.TrimPrefix(p, absPath+"/")
+	}
+
 	p = filepath.ToSlash(p)
 	if filepath.VolumeName(p) == "" {
 		if len(p) > 0 {
