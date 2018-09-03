44e7314532e3ff32148d6d37a967c4df95a4601c
Xabier Eizmendi <xeizmendi@gmail.com>
2018-09-03 17:28:31 +0200

    Fix escaped backslash

--- cleanpath_windows.go
+++ cleanpath_windows.go
@@ -8,6 +8,9 @@ import (
 // Makes sure we have a clean Windows absolute path to work with
 // If the path is not absolute we assume it's on C:\
 func cleanPath(p string) string {
+	// Hack remove double escaped backslash
+	p = strings.Replace(p, "\\\\", "\\", -1)
+
 	// Hack to make path sent by sftp client work in Windows
 	if absPath, err := filepath.Abs("."); err == nil {
 		p = strings.TrimPrefix(p, absPath+"/")
